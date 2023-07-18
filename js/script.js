a6fd0b2435eddd1d4bd4fbb04002086d15a75a36
Zander Sacay <zandersacay@gmail.com>
2023-07-19 00:42:24 +0800

    Created function to create grid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const easelButton = document.querySelector(".easel-button");
 const easel = document.querySelector(".easel");
+const grid = document.querySelector(".grid");
+
+function createGrid(numOnSide) {
+    let total = numOnSide ** 2;
+    let dimension = 800 / numOnSide - 2;
+    for (let i=0; i<total; i++) {
+        let pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.width = `${dimension}px`;
+        pixel.style.height = `${dimension}px`;
+        grid.appendChild(pixel);
+    }
+}
 
 easelButton.addEventListener("click", () => {
     if (!easel.style.translate) {
