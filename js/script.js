8f6731324f64341e32f2a32ffa5896cff1a58e5b
Zander Sacay <zandersacay@gmail.com>
2023-07-19 00:46:22 +0800

    Added click event listener to new grid button to call createGrid function based on grid size input value

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const easelButton = document.querySelector(".easel-button");
 const easel = document.querySelector(".easel");
 const grid = document.querySelector(".grid");
+const gridSizeInput = document.querySelector(".grid-size-input");
+const newGridButton = document.querySelector(".new-grid-button");
+
+newGridButton.addEventListener("click", () => {
+    let numOnSide = gridSizeInput.value;
+    createGrid(numOnSide);
+})
 
 function createGrid(numOnSide) {
     let pixels = document.querySelectorAll(".pixel");
